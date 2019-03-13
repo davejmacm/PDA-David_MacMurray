771156b8d92be6c090135e38b12e06f78da33a29
davejmacm <davejmacm@gmail.com>
2019-03-13 16:05:35 +0000

    unit tests

--- Testing Exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Testing Exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,4 +11,28 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  it('it adds numbers together', function(){
+    calculator.previousTotal = 4
+    calculator.add(1)
+    assert.equal(calculator.runningTotal, 5)
+  })
+
+  it('it subtracts numbers', function(){
+    calculator.previousTotal = 7
+    calculator.subtract(4)
+    assert.equal(calculator.runningTotal, 3)
+  })
+
+  it('it multiples numbers', function(){
+    calculator.previousTotal = 5
+    calculator.multiply(3)
+    assert.equal(calculator.runningTotal, 15)
+  })
+
+  it('it divides numbers', function(){
+    calculator.previousTotal = 21
+    calculator.divide(3)
+    assert.equal(calculator.runningTotal, 7)
+  })
+
 });
